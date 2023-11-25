858af88be246fd85380b0cfd733d47e43875dfed
cheesehome55 <133651346+cheesehome55@users.noreply.github.com>
2023-11-25 06:53:39 +0600

    Update permissions.py

--- cats/permissions.py
+++ cats/permissions.py
@@ -1,1 +1,19 @@
+from rest_framework import permissions
 
+
+class OwnerOrReadOnly(permissions.BasePermission):
+
+    # Определение права на уровне запроса или пользователя
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
+    # Определение права на уровне объекта
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class ReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
